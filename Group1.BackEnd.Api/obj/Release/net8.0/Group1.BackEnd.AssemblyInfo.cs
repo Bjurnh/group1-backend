f0370f03dd5d851a5430eb5e6926ef439d721c39
Aidrian Camariosa <camariosanielaidrian@gmail.com>
2025-03-11 07:43:44 +0800

    with .gitignore

--- Group1.BackEnd.Api/obj/Release/net8.0/Group1.BackEnd.AssemblyInfo.cs
+++ Group1.BackEnd.Api/obj/Release/net8.0/Group1.BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group1.BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82dab184b3b8cf9a6ec8054d63cba6299fe991d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group1.BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group1.BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Group1.BackEnd.Infrastructure/obj/Release/net8.0/Group1.BackEnd.Infrastructure.AssemblyInfo.cs
+++ Group1.BackEnd.Infrastructure/obj/Release/net8.0/Group1.BackEnd.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group1.BackEnd.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88150671f04667b3443aa3889234d43bbb0a09d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82dab184b3b8cf9a6ec8054d63cba6299fe991d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group1.BackEnd.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group1.BackEnd.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
